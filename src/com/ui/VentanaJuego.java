457be43e839e79900197f5ab860d70723c97811b
paste-fiuba <mpasteloff@fi.uba.ar>
2025-10-29 20:15:04 -0300

    Borro algunos comentarios

--- src/com/ui/VentanaJuego.java
+++ src/com/ui/VentanaJuego.java
@@ -10,28 +10,21 @@ public class VentanaJuego extends JFrame {
 
     /**
      * Constructor.
-     * @param panel El PanelJuego (lienzo) que debe mostrar.
+     * @param panel El PanelJuego que debe mostrar.
      */
     public VentanaJuego(PanelJuego panel) {
-        // 1. Título de la ventana
         setTitle("Rolgar II");
 
-        // 2. Acción al cerrar (terminar el programa)
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // 3. Impedimos que el usuario cambie el tamaño
         setResizable(false);
 
-        // 4. Añadimos el panel a la ventana
         add(panel);
 
-        // 5. Ajustamos el tamaño de la ventana al contenido 
         pack();
 
-        // 6. Centramos la ventana en la pantalla
         setLocationRelativeTo(null);
 
-        // 7. Hacemos visible la ventana
         setVisible(true);
     }
 }
